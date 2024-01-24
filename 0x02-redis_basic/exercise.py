957ee68a291e9508495e413f8529aed709823f40
Hilly-man <hilkiahe@gmail.com>
2024-01-24 22:10:24 +0000

    Read from redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,7 @@
 import uuid
 import redis
 from typing import Union
+from typing import Callable
 
 class Cache:
     def __init__(self):
@@ -11,3 +12,14 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn: Callable = None):
+        value = self.redia_client.get(key)
+        if value is not None:
+            return fn(value) if fn else value
+
+    def get_str(self, key):
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key):
+        return self.get(key, fn=int)
